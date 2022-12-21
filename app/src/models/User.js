c16113373d372f56994ee1d5fa475ac18a6931e3
msdou46 <msdou46@gmail.com>
2022-12-21 20:13:44 +0900

    fs 모듈을 통한 회원가입 기능 구현

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -20,9 +20,15 @@ class User {
         return {success: false, message: "존재하지 않는 아이디입니다."};
     }
 
-    register() {
+    async register() {
         const client = this.body;
-        UserStorage.save(client)
+        try {
+            const response = await UserStorage.save(client);
+            return response
+        } catch (err) {
+            return {success: false, message: err};
+        }
+
     }
 }
 
--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -15,16 +15,26 @@ class UserStorage {
 
         return userInfo
     }
-    
-    static getUsers (...fields) {
-        //const users = this.#users;
-        const newUsers = fields.reduce((newUsers, field) => {
+
+    static #getUsers (data, isAll, fields) {
+        const users = JSON.parse(data);
+        if (isAll) return users;
+
+        const newUsers = fields.reduce((newUsers, field) => {   // 전체 데이터 중 일부 필드만 가져오고 싶을 때.
             if (users.hasOwnProperty(field)) {
                 newUsers[field] = users[field];
             }
             return newUsers;
-        }, {});
+        }, {}); // 전체 데이터 타입. 객체로 반환한다.
         return newUsers;
+    }
+    
+    static getUsers (isAll, ...fields) {
+        return fs.readFile("./src/databases/users.json")
+            .then((data) => {
+                return this.#getUsers(data, isAll, fields);
+            })
+            .catch(console.error);
     }
 
     static getUserInfo (id) {
@@ -35,8 +45,19 @@ class UserStorage {
             .catch(console.error);
     }
 
-    static save (userInfo) {
-
+    static async save (userInfo) {
+        const users = await this.getUsers(true);
+        
+        if(users.id.includes(userInfo.id)) {
+            throw "이미 존재하는 아이디입니다.";
+        }
+        users.id.push(userInfo.id);
+        users.name.push(userInfo.name);
+        users.psword.push(userInfo.psword);
+        // 데이터 저장. 그냥 저장하면 기존의 데이터들을 전부 덮어 씌워 버리기 때문에, 일단 모든 데이터를 가져와서 
+        // 새로 추가되는 유저 정보를 더한 다음에, 그렇게 완성된 총 데이터를 다시 파일에 넣어야 해.
+        fs.writeFile("./src/databases/users.json", JSON.stringify(users));
+        return { success: true };
     }
 }
 
