6ea8f2b48b0fddf71d96c85893d800b8e13eb8c7
msdou46 <msdou46@gmail.com>
2022-12-21 14:06:26 +0900

    프런트 단 js 파일 연결 확인

--- app/app.js
+++ app/app.js
@@ -8,7 +8,7 @@ const app = express();
 const home = require("./src/routes/home");  // index.js 를 가져와.
 
 // 앱 세팅
-app.set("views", "./src/views");
+app.set("views", "./src/views");    // ./ 는 현재 디렉토리 위치를 뜻해.
     /*
     화면 view단에 해당하는 부분을 처리해줄 수 있는 view 엔진을 세팅.
     첫 인자는 "views" 라는 속성을 set 해주겠단 거고,
@@ -19,6 +19,7 @@ app.set("view engine", "ejs")
     ./views 안에 생성될 html의 코드들을 어떤 엔진으로 해석할지를 정해줄 수 있어.
     ejs 는 html 과 굉장히 흡사한 핸진.
     */
+app.use(express.static(`${__dirname}/src/public`))
 
 app.use("/", home);
 
