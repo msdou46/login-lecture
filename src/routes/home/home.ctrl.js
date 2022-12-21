e03a80a1b4c0b4e1388c80c177c128709ba6c1b7
msdou46 <msdou46@gmail.com>
2022-12-21 13:32:31 +0900

    src폴더로 소스코드 분류

--- src/routes/home/home.ctrl.js
+++ src/routes/home/home.ctrl.js
@@ -2,7 +2,7 @@
 
 const home = (req, res) => {
     res.render("home/index")
-    //  app.set("views", "./views") 로 폴더 경로를 설정해놨기 때문에 home/index 만 찾으면 돼.
+    //  app.set("views", "./views") 로 폴더 경로를 설정해놨기 때문에 home/index 만 찾으면 돼. app.js 기준이야.
 }
 
 const login = (req, res) => {
